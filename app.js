fd3e24d5df51e9d633e449c200165ec6be336df8
uo251119 <uo251119@uniovi.es>
2021-04-29 11:44:31 +0200

    SDI-2021-207-10.3-SW Cliente JQuery + AJAX.

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ let https = require('https');
 let jwt = require('jsonwebtoken');
 app.set('jwt',jwt);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
 // routerUserSession
 var routerUserSession = express.Router();
 routerUserSession.use(function(req, res, next) {
